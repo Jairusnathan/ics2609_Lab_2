719d8577c387db4c81bedc66584e43f9d89d5c94
Jairusnathan <jnvalenton21@gmail.com>
2025-09-06 13:02:45 +0800

    courseRoutes.js

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const ctrl = require('../controllers/courseController');
 
 router.get('/courses', ctrl.getCourses);
-router.get('/courses/:id', ctrl.getCourseById);   // singular "Course"
-router.post('/courses', ctrl.createCourse);      // singular "Course"
-router.put('/courses/:id', ctrl.updateCourse);   // singular "Course"
-router.delete('/courses/:id', ctrl.deleteCourse); // singular "Course"
+router.get('/courses/:id', ctrl.getCourseById);  
+router.post('/courses', ctrl.createCourse);      
+router.put('/courses/:id', ctrl.updateCourse);   
+router.delete('/courses/:id', ctrl.deleteCourse); 
 
 module.exports = router;
+
